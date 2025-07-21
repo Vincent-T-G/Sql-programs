901371c0cd8b405c435566513f54e365c98ddb32
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-07-21 19:11:53 +0300

    Added a new sql exercise script

---
+++ Learning programs/Exercises/exercise_07.sql
@@ -0,0 +1,17 @@
+USE sql_store;
+
+SELECT *
+FROM customers
+WHERE first_name REGEXP 'elka|ambur';
+
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'ey$|on$';
+
+SELECT *
+FROM customers
+WHERE last_name REGEXP '^my|se';
+
+SELECT *
+FROM customers
+WHERE last_name REGEXP 'b[ru]';
